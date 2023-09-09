61f664063ecff76758c66f66ef6b293cc4827506
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-09-10 00:45:01 +0530

    Update app.js

--- App/app.js
+++ App/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const helmet = require("helmet");
 const nodemailer = require("nodemailer");
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({ origin: "*" }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
